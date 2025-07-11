29797d980b4d351a4b3972b000bf68e75bb95e57
someshwarsholkar <someshwarsholkar22@gmail.com>
2025-07-11 21:57:53 +0530

    dist updated

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import reduxStore from './redux/store.js'
+import { StrictMode } from 'react'
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-  <Provider store={reduxStore}>
-    <App />
-  </Provider>,
-  // </StrictMode>,
+  <StrictMode>
+    <Provider store={reduxStore}>
+      <App />
+    </Provider>,
+  </StrictMode>,
 )
